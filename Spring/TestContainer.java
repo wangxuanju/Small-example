9f8d742481720d23ea4ad7667f95303092fc859c
wangxuanju <wangxuanju@users.noreply.github.com>
2018-10-30 20:51:42 +0800

    Create TestContainer.java

---
+++ Spring/TestContainer.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestContainer {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		Header header = context.getBean("header",StrightHeader.class);
+		System.out.println(header.getInfo());
+		header.doWork();
+		
+		
+		((ConfigurableApplicationContext)context).close();
+	}
+
+}
